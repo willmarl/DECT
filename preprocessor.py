d717d83617fe4d306d2911e19d38492d120979ed
William Marlette <williammarlette3@gmail.com>
2025-09-22 19:13:07 -0400

    add script to convert pdf to images

---
+++ preprocessor.py
@@ -0,0 +1,43 @@
+from pdf2image import convert_from_bytes, convert_from_path
+from pathlib import Path
+import os
+
+def fresh_pdf_folders():
+    p = Path("pdf2img")
+    if p.exists():
+        for folder in p.iterdir():
+            for file in folder.iterdir():
+                file.unlink()
+            folder.rmdir()
+    else:
+        p.mkdir()
+
+def make_folder(folder_name):
+    p = Path(f"pdf2img/{folder_name}")
+    p.mkdir()
+
+def pdf_to_images(pdf_bytes):
+    """
+    this is very inefficient as any file upload or remove will restart the whole process
+    will optimize later
+    """
+    fresh_pdf_folders() # clear old folders
+    fileList = []
+    # get list of filenames
+    for file in pdf_bytes:
+        fileList.append(os.path.basename(file.name))
+
+    # save images to respective folders
+    for i, fileName in enumerate(fileList):
+        make_folder(fileName)
+        # read the actual file content from the Gradio file object
+        with open(pdf_bytes[i].name, 'rb') as file:
+            file_bytes = file.read()
+        images = convert_from_bytes(file_bytes, dpi=300)
+        for j, img in enumerate(images):
+            img.save(f"pdf2img/{fileName}/{j+1}.png", "PNG")
+
+    if len(fileList) > 0:
+        return True
+    else:
+        return False
